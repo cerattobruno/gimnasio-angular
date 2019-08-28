6103e3eaafc943814c3e9fc8b4cd3bf8cb22e005
cerattobruno <cerattobruno@gmail.com>
2019-08-28 13:31:11 -0300

    Se creo la estructura princial del sistema y la pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { SociosComponent } from './components/socios/socios.component';
+import { AdministracionComponent } from './components/administracion/administracion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'principal', component: PrincipalComponent},
+  {path: 'socios', component: SociosComponent},
+  {path: 'administracion', component: AdministracionComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'principal'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { SociosComponent } from './components/socios/socios.component';
+import { AdministracionComponent } from './components/administracion/administracion.component';
+import { IngresoComponent } from './components/shared/ingreso/ingreso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PrincipalComponent,
+    SociosComponent,
+    AdministracionComponent,
+    IngresoComponent
   ],
   imports: [
     BrowserModule,
